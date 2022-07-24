8854e53e9c24e97a159b4802c4622b5287368c88
night <yseddiq@gmail.com>
2022-07-24 13:44:45 -0400

    created and appended a radio button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,18 @@
 //this variable will hold the score, and eventually get stored in localStorage.
 var score=0;
+
 //counter for the timer.
 var timeLeft= 300
+
+//this will subtract 50 seconds from the timer for the wrong answer. 
+var penalty = function(){
+    timeLeft -=50
+}
+
+//following code will make radio buttons that will be appended to the HTML.
+
+var radioButton1 = document.createElement("Input");
+    radioButton1.setAttribute("type","radio");
 
 //this is an array that will hold the answers. the idea is that all the index values of the question, 
 //options, and answers array will match up, which will make it easier to create iterate in an 
@@ -12,9 +23,7 @@ var answerArray=["Batman", "Selina Kyle", "Robin"];
 //"option A" will be the value for all the A options for each of the questions. 
 //all values are placeholders.
 
-    QuestionZeroOptions= ["Batman", "Two-Face", "Hush", "Grant Morrison"]
-    QuestionOneOptions=["Mos Eisley", "Selina Kyle", "Gail Simone", "Hally Berry"]
-    QuestionTwoOptions=["Signal", "Dick Tracy", "Robin", "Nightwing"]
+    var buttonAArray =["batman", "gail simone", "Dick Clark"]
 
 //this is an array that will hold the questions. all values are placeholders.
 var questionsArray=["What is the alter ego of Bruce Wayne?", "What is Catwoman's real identity?", "Who is Batman's most well known sidekick?"];
@@ -50,6 +59,7 @@ var countdown = function(){
 
 //once the start button is clicked, this function will run and start the quiz. 
 var startQuiz = function(){
+    countdown();
     $(".quiz-description").text("");
     var questionNumber=0
     var optionsDiv = document.querySelector(".options");
@@ -64,7 +74,7 @@ var startQuiz = function(){
     // removes the start button from the page.    
     removeButton.remove();
     $(".Next").on('click', function(){
-        countdown();
+
         nextButton.innerHTML="next question"
         console.log(questionNumber)
         
@@ -74,10 +84,10 @@ var startQuiz = function(){
         //iterates through each question, and option on clicking the Next Button.
         if(questionNumber<questionsArray.length){
             $(".questions").append("<div>" + questionsArray[questionNumber] + "</div>");
+            $(".button-A").append([radioButton1 , "<div>"+buttonAArray[questionNumber]+"</div>"])
             questionNumber++;
 
             
-
         }
     })   
 
